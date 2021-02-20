d915533be0ba9b8da0b377767d6638a63fd952f7
bocai <1224680008@qq.com>
2021-02-20 13:51:55 +0800

    feat: update esctl-0.0.5

--- esctl.rb
+++ esctl.rb
@@ -4,8 +4,8 @@
 class Esctl < Formula
   desc "CLI for managing the ElasticSearch cluster"
   homepage ""
-  url "https://github.com/zhb127/esctl/archive/v0.0.4.tar.gz"
-  sha256 "fc9adfddcbc34607dabc669fec8ad759917d0c2884feba4ee384659919de6ee2"
+  url "https://github.com/zhb127/esctl/archive/v0.0.5.tar.gz"
+  sha256 "239090b56266b8b23c1d578decd56e7e1677f89a410500d829332d007c1889c6"
   license "GPL-3.0"
 
   depends_on "go" => :build
