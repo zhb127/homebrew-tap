478fef32495c15c61e9ce201726f96bbae076425
zhb127 <1224680008@qq.com>
2021-02-26 09:18:46 +0000

    esctl: add 0.0.15 bottle.

--- esctl.rb
+++ esctl.rb
@@ -5,6 +5,13 @@ class Esctl < Formula
   sha256 "6c5b705bd0eed28edcc4d60c0391892beda10fc4ae37006c09b5edbf2eb34ac4"
   license "GPL-3.0"
 
+  bottle do
+    root_url "https://github.com/zhb127/homebrew-tap/releases/download/esctl-0.0.15"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "21a7881564ce82919283a544a3841a0a592dfd38e6ea1180913c98d6dd96092a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9ef0e02f72887175b60acacf34ec63aa5050b9b461be23c28588295a3f197c68"
+  end
+
   depends_on "go" => :build
 
   def install
