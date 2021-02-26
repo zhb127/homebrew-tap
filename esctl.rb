d196ff752712c3d5a1a1fe1170c3be0751f47741
bocai <1224680008@qq.com>
2021-02-26 12:22:27 +0800

    fix: upgrade esctl and fix brew test error

--- esctl.rb
+++ esctl.rb
@@ -1,8 +1,8 @@
 class Esctl < Formula
   desc "CLI for managing the ElasticSearch cluster"
   homepage "https://github.com/zhb127/esctl"
-  url "https://github.com/zhb127/esctl/archive/v0.0.13.tar.gz"
-  sha256 "ac646dcec039e4dbd15bd1ad2745f5a343c559858499cf5886940a47bfa8210a"
+  url "https://github.com/zhb127/esctl/archive/v0.0.15.tar.gz"
+  sha256 "6c5b705bd0eed28edcc4d60c0391892beda10fc4ae37006c09b5edbf2eb34ac4"
   license "GPL-3.0"
 
   depends_on "go" => :build
@@ -14,6 +14,6 @@ class Esctl < Formula
 
   test do
     ENV.delete "GITHUB_TOKEN"
-    assert_match "no Github token found", shell_output(bin/"esctl", 255)
+    assert_match "esctl controls the ElasticSearch cluster manager", shell_output(bin/"esctl", 0)
   end
 end
