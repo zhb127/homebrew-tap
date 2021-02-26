885302cdca1456056f11475486f19fdd36c144e6
bocai <1224680008@qq.com>
2021-02-26 10:47:09 +0800

    fix: brew style error

--- esctl.rb
+++ esctl.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Esctl < Formula
   desc "CLI for managing the ElasticSearch cluster"
   homepage ""
