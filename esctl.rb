efd4a5aa633615e6b950181742d3c7c6899e97c1
bocai <1224680008@qq.com>
2021-03-02 15:00:41 +0800

    feat: update esctl v0.0.17

--- esctl.rb
+++ esctl.rb
@@ -1,8 +1,8 @@
 class Esctl < Formula
   desc "CLI for managing the ElasticSearch cluster"
   homepage "https://github.com/zhb127/esctl"
-  url "https://github.com/zhb127/esctl/archive/v0.0.15.tar.gz"
-  sha256 "6c5b705bd0eed28edcc4d60c0391892beda10fc4ae37006c09b5edbf2eb34ac4"
+  url "https://github.com/zhb127/esctl/archive/v0.0.17.tar.gz"
+  sha256 "f2d40a2d89dcaee0e979ac872e6d8f4427b1317117603913750436ae8047852d"
   license "GPL-3.0"
 
   bottle do
