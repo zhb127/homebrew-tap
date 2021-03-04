bf161c74522b135c3f01d025b8f2040b922e0e7c
bocai <1224680008@qq.com>
2021-03-04 17:10:15 +0800

    fix: esctl.rb sha256
    Closes #11.
    
    Signed-off-by: zhb127 <1224680008@qq.com>

--- esctl.rb
+++ esctl.rb
@@ -2,7 +2,7 @@ class Esctl < Formula
   desc "CLI for managing the ElasticSearch cluster"
   homepage "https://github.com/zhb127/esctl"
   url "https://github.com/zhb127/esctl/archive/v0.0.18.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "ad19f52b9425f3bc2bdc8b14279f630d7793723ab43a62b669f011ab1af8f559"
   license "GPL-3.0"
 
   depends_on "go" => :build
