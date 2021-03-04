23753fd97f8c19140504f3aa03c0902a87562ae2
bocai <1224680008@qq.com>
2021-03-04 16:39:38 +0800

    feat: upgrade esctl 0.0.18

--- esctl.rb
+++ esctl.rb
@@ -1,15 +1,9 @@
 class Esctl < Formula
   desc "CLI for managing the ElasticSearch cluster"
   homepage "https://github.com/zhb127/esctl"
-  url "https://github.com/zhb127/esctl/archive/v0.0.17.tar.gz"
-  sha256 "f2d40a2d89dcaee0e979ac872e6d8f4427b1317117603913750436ae8047852d"
+  url "https://github.com/zhb127/esctl/archive/v0.0.18.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "GPL-3.0"
-
-  bottle do
-    root_url "https://github.com/zhb127/homebrew-tap/releases/download/esctl-0.0.17"
-    sha256 cellar: :any_skip_relocation, catalina:     "8aca6f1afc91de86be755385b0a8976d0d8808245631542ad66e64811439516c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a03751e8714c270919539d2294e445ed24f6b2e296f78ba1369329edd35ebe9c"
-  end
 
   depends_on "go" => :build
 
