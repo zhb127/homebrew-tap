7d45bc25eb4605d5e154307be33835345053f871
bocai <1224680008@qq.com>
2021-03-15 23:05:39 +0800

    feat: upgrade zhb127 to 0.2

--- Formula/zhb127.rb
+++ Formula/zhb127.rb
@@ -1,7 +1,7 @@
 class Zhb127 < Formula
   desc "Echo helloworld"
   homepage "https://github.com/zhb127/homebrew-tap"
-  url "https://github.com/zhb127/homebrew-helloworld/archive/0.1.tar.gz"
+  url "https://github.com/zhb127/homebrew-helloworld/archive/0.2.tar.gz"
   sha256 "dd26eb15512fc5418e9a522746e87151f37da4bdae60dc5483aba10393d125c7"
   license ""
 
@@ -12,6 +12,6 @@ class Zhb127 < Formula
   end
 
   test do
-    assert_match "Hello World", shell_output(bin/"zhb127")
+    assert_match "Hello World 0.2", shell_output(bin/"zhb127")
   end
 end
