ab9e7ef3c047f65f3f206efc27d23d361089eebb
bocai <1224680008@qq.com>
2021-03-31 17:45:19 +0800

    feat: esctl v0.0.19

---
+++ esctl.rb
@@ -0,0 +1,17 @@
+class Esctl < Formula
+  desc "CLI for managing the ElasticSearch cluster"
+  homepage "https://github.com/zhb127/esctl"
+  url "https://github.com/zhb127/esctl/archive/v0.0.19.tar.gz"
+  sha256 "e4dae1688bf8191bd4e63fa4b018ad27ce9655cb08c673a420b47ef3c40c2a6d"
+  license "GPL-3.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    assert_match "esctl controls the ElasticSearch cluster manager", shell_output(bin/"esctl")
+  end
+end
